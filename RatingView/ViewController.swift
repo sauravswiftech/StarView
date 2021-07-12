cb39b36bcf02960bcdfa5a11832640a34fa82424
skumarTilaLabs <skumar1@tila.com>
2021-07-12 14:55:07 +0530

    Initial Commit

---
+++ RatingView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RatingView
+//
+//  Created by FPTECHS068 on 12/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
